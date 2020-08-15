9feedb7b3623879f6a115374ecb6e077cc200c87
pehalute <ryutonakamura1246@gmail.com>
2020-08-16 08:27:50 +0900

    就航空港・使用機材変更

--- public/index.js
+++ public/index.js
@@ -3,20 +3,125 @@ const app = new Vue({
   data: {
     airports: [
       {
-        name: "東京（羽田）",
-        code: "RJTT"
+        name: "バンクーバー国際空港",
+        code: "CYVR"
       },{
-        name: "大阪（関西）",
-        code: "RJBB"
+        name: "フランクフルト空港",
+        code: "EDDF"
       },{
-        name: "台湾（桃園）",
+        name: "ミュヘン空港",
+        code: "EDDM"
+      },{
+        name: "ロンドン・ヒースロー空港",
+        code: "EGLL"
+      },{
+        name: "ジョン・F・ケネディ国際空港（ニューヨーク）",
+        code: "KJFK"
+      },{
+        name: "ロサンゼルス国際空港",
+        code: "KLAX"
+      },{
+        name: "サンフランシスコ国際空港",
+        code: "KSFO"
+      },{
+        name: "シャルル・ド・ゴール国際空港（パリ）",
+        code: "LFPG"
+      },{
+        name: "ペナン国際空港",
+        code: "PGUN"
+      },{
+        name: "ダニエル・K・イノウエ国際空港 (ホノルル)",
+        code: "PHNL"
+      },{
+        name: "台湾桃園国際空港",
         code: "RCTP"
       },{
-        name: "サンフランシスコ",
-        code: "KSFO"
-      },
+        name: "関西国際空港",
+        code: "RJBB"
+      },{
+        name: "新千歳空港",
+        code: "RJCC"
+      },{
+        name: "函館空港",
+        code: "RJCH"
+      },{
+        name: "福岡空港",
+        code: "RJFF"
+      },{
+        name: "鹿児島空港",
+        code: "RJFK"
+      },{
+        name: "新田原基地",
+        code: "RJFN"
+      },{
+        name: "大分空港",
+        code: "RJFO"
+      },{
+        name: "長崎空港",
+        code: "RJFU"
+      },{
+        name: "中部国際空港",
+        code: "RJGG"
+      },{
+        name: "名古屋飛行場",
+        code: "RJNA"
+      },{
+        name: "広島空港",
+        code: "RJOA"
+      },{
+        name: "大阪国際空港（伊丹）",
+        code: "RJOO"
+      },{
+        name: "青森空港",
+        code: "RJSA"
+      },{
+        name: "新潟空港",
+        code: "RJSN"
+      },{
+        name: "仙台空港",
+        code: "RJSS"
+      },{
+        name: "八丈島空港",
+        code: "RJTH"
+      },{
+        name: "入間基地",
+        code: "RJTJ"
+      },{
+        name: "大島空港",
+        code: "RJTO"
+      },{
+        name: "三宅島空港",
+        code: "RJTQ"
+      },{
+        name: "羽田国際空港",
+        code: "RJTT"
+      },{
+        name: "仁川国際空港（ソウル）",
+        code: "RKSI"
+      },{
+        name: "那覇空港",
+        code: "ROAH"
+      },{
+        name: "石垣島空港",
+        code: "ROIG"
+      },{
+        name: "宮古空港",
+        code: "ROMY"
+      },{
+        name: "ウラジオストク空港",
+        code: "UHWW"
+      },{
+        name: "香港国際空港",
+        code: "VHHH"
+      },{
+        name: "上海虹橋国際空港",
+        code: "ZSSS"
+      },{
+        name: "シンガポール・チャンギ空港",
+        code: "WSSS"
+      }
     ],
-    airplanes: ["B787-9", "A350-900", "B777-200ER", "B787-10"],
+    airplanes: ["A318", "A320", "A321", "A359", "B738", "B742", "B748", "B772", "B77W", "B789", "Q400", "C172"],
     members: [
       {
         name: "乱健",
@@ -36,11 +141,11 @@ const app = new Vue({
         name: "村上太郎",
         id: "murakami_taro1",
       },{
-        name: "誰かの友達",
+        name: "TAKAHIRO",
         id: "DarekanoTo_JPN",
       },{
-        name: "pehalute",
-        id: "pehalute",
+        name: "RYUTO",
+        id: "ryuto1246",
       },
     ]
   }
